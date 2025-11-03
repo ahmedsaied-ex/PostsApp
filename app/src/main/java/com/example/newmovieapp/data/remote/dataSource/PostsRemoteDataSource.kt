da6160ac4d9d411ede4ea49504c1a0515558c555
ahmed.saied <ahmed.saied@expertapps.com.sa>
2025-11-03 14:58:27 +0200

    HANDEL ERROR

--- app/src/main/java/com/example/newmovieapp/data/remote/dataSource/PostsRemoteDataSource.kt
+++ app/src/main/java/com/example/newmovieapp/data/remote/dataSource/PostsRemoteDataSource.kt
@@ -12,15 +12,8 @@ class PostsRemoteDataSource @Inject constructor(
     private val api: PostsApi
 ) {
     suspend fun getPosts(page: Int, limit: Int): List<PostDto> {
-        return try {
-            api.getPosts(page, limit)
-        } catch (e: IOException) {
-            throw IOException("Network error: ${e.localizedMessage ?: "check your connection"}")
-        } catch (e: HttpException) {
-            throw HttpException(e.response()!!)
-        } catch (e: Exception) {
-            throw Exception("Unexpected error: ${e.localizedMessage ?: "unknown"}")
-        }
+        return api.getPosts(page, limit)
+
     }
     suspend fun createPost(post: PostDto): PostDto {
         return try {
